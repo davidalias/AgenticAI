9b0b61a09d5bac29c4d70bebe6608e91ec73a579
Davis Boban <davis.boban@atos.net>
2025-10-30 09:48:24 +0530

    RAG search with LLM

--- RAG/app.py
+++ RAG/app.py
@@ -1,7 +1,7 @@
 from src.data_loader import load_all_documents
 from src.embedding import EmbeddingPipeline
 from src.vectorstore import FaissVectorStore
-
+from src.search import RAGSearch
 
 #Example usage
 
@@ -20,6 +20,15 @@ if __name__ == "__main__":
     store = FaissVectorStore("faiss_store")
     store.build_from_documents(docs)"""
 
+    """
+    
+    For final output, no need for this part. This part only needed to check vectorstore pipeline working
+
     store = FaissVectorStore("faiss_store")
     store.load()
     print(store.query("What is RAG?", top_k =3))
+"""
+    rag_search = RAGSearch()
+    query = "What is RAG?"
+    summary = rag_search.search_and_summarize(query, top_k=3)
+    print("Summary:", summary)
